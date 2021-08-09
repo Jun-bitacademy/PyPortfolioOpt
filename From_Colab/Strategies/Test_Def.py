a1ffaf403b1a7ba992480838993986241d067d52
jun-bitacademy <jeongjiyoon93@gmail.com>
2021-08-09 16:35:55 +0900

    8.9.21 - 1차 함수화 및 클래스화

---
+++ From_Colab/Strategies/Test_Def.py
@@ -0,0 +1,24 @@
+# from IPython.core.interactiveshell import InteractiveShell
+# InteractiveShell.ast_node_interactivity = "all"
+
+from def_InstantRaise_sharpe import InstanceRaise_sharpe
+from def_InstantRaise_eff import InstanceRaise_eff
+# from def_Momentum1mo_sharpe import Momentum1mo_sharpe
+# from def_Momentum1mo_eff import Momentum1mo_eff
+# from def_Momentum3mos_sharpe import Momentum3mos_sharpe
+# from def_Momentum3mos_eff import Momentum3mos_eff
+# from def_Updown_sharpe import Updown_sharpe
+# from def_Updown_eff import Updown_eff
+# from def_Dual_sharpe import Dual_sharpe
+# from def_Dual_eff import Dual_eff
+
+InstanceRaise_sharpe()# 16 min / Expected annual return: 33.9% / Annual volatility: 22.5% / Sharpe Ratio: 1.41 두개 합쳐서 10분
+InstanceRaise_eff() # Expected annual return: 42.3% / Annual volatility: 30.0% / Sharpe Ratio: 1.34
+# Momentum3mos_sharpe() # 3 min 57 sec / Expected annual return: 74.5% / Annual volatility: 31.0% / Sharpe Ratio: 2.34 두개 합쳐서 10분
+# Momentum3mos_eff() # Expected annual return: 72.1% / Annual volatility: 30.0% / Sharpe Ratio: 2.34
+# Momentum1mo_sharpe() # 4 min 40 sec / Expected annual return: 107.3% / Annual volatility: 36.0% / Sharpe Ratio: 2.92 / [54, 2, 106, 8, 5, 172, 35, 8, 38] 두개 합쳐서 10분
+# Momentum1mo_eff() # Expected annual return: 85.4% / Annual volatility: 30.0% / Sharpe Ratio: 2.78 / [170, 27, 9, 75, 6, 7, 111, 88, 191, 35, 17, 38]
+# Updown_sharpe() # 4 min 20 sec / 'Expected annual return: 4166162.7% / Annual volatility: 30.7% / Sharpe Ratio: 135822.98'
+# Updown_eff() # 'Expected annual return: 4100883.6% / Annual volatility: 30.0% / Sharpe Ratio: 136620.65'
+# Dual_sharpe() # 40 sec
+# Dual_eff()
